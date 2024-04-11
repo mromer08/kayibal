217c315f0145173b5fd532e945e226e31228af61
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2024-04-11 02:30:59 -0600

    add: publications router

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PublicationController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -23,5 +24,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('publications', PublicationController::class);
 
 require __DIR__.'/auth.php';
